a0ed6d2826caad508ad05597e93d1d42f93ba5da
G3RGES <Georgios.Nashaat@gmail.com>
2023-11-04 16:18:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+import React, { useState, useEffect } from "react";
+
 function App() {
+  const [images, setImges] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App max-w-sm rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full" />
@@ -20,6 +35,26 @@ function App() {
             400
           </li>
         </ul>
+      </div>
+      <div className="px-6 py-4">
+        <span
+          className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm 
+        font-semibold text-gray-700 mr-2"
+        >
+          #tag1
+        </span>
+        <span
+          className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm 
+        font-semibold text-gray-700 mr-2"
+        >
+          #tag2
+        </span>
+        <span
+          className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm 
+        font-semibold text-gray-700 mr-2"
+        >
+          #tag3
+        </span>
       </div>
     </div>
   );
